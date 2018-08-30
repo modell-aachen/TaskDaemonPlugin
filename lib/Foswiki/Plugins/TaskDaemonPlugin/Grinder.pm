5a6b59030048299d92b9e2583638d63695056dc0
Ingo Krützen <kruetzen@modell-aachen.de>
2018-08-30 10:19:37 +0200

    id:5804 add general grinder

--- lib/Foswiki/Plugins/TaskDaemonPlugin/Grinder.pm
+++ lib/Foswiki/Plugins/TaskDaemonPlugin/Grinder.pm
@@ -1,10 +1,40 @@
 use strict;
 use warnings;
 
+use JSON;
+use Encode qw(encode);
+
 {
     handle_message => sub {
-        my ($host, $t, $hdl, $run_engine, $json) = @_;
-        # process message...
+        my ($host, $type, $hdl, $run_engine, $json) = @_;
+
+        eval {
+            my $data = from_json($json->{data});
+            push (@ARGV, encode('UTF-8', "/" . $data->{webtopic})); # XXX
+            $Foswiki::engine->{user} = $data->{user};
+            $run_engine->();
+            pop @ARGV;
+        };
+        if($@) {
+            print STDERR $@;
+        }
+
         return {};
+    },
+    engine_part => sub {
+        my ($session, $type, $data, $caches) = @_;
+
+        my $json = from_json($data);
+        if($json->{callback}){
+            eval {
+                "$json->{callback}"->grinder($session, $type, $data, $caches);
+            };
+            if($@) {
+                print STDERR $@;
+                Foswiki::Func::writeWarning("Grinder called, but the callback-Plugin has no grinder-method ($@)");
+            }
+        } else {
+            Foswiki::Func::writeWarning("Grinder called, but no callback was found");
+        }
     },
 };
